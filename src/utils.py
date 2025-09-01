32a5e7a83d0b6af2334966057f918c265744fe01
Parag3399 <parag.gore14@gmail.com>
2025-09-01 12:42:05 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -17,4 +18,20 @@ def save_object(file_path, obj):
             dill.dump(obj, file)
 
     except Exception as e:
-        raise CustomException(e, sys)   
+        raise CustomException(e, sys)  
+
+def evalute_model(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluate multiple machine learning models and return their R2 scores.
+    """
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys) 
