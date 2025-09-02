b0aa6f9112eaffd9ccdcfeca59e74ca2ed5179fa
Parag3399 <parag.gore14@gmail.com>
2025-09-02 13:25:41 +0530

    HTML and Flask Ingration

--- src/utils.py
+++ src/utils.py
@@ -35,3 +35,11 @@ def evalute_model(X_train, y_train, X_test, y_test, models):
         return report
     except Exception as e:
         raise CustomException(e, sys) 
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e, sys)
+        
